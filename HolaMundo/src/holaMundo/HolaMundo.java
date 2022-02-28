9ec41eb1b646c925e01b9b3051ea222cf55629ca
debian <debian@debian>
2022-02-28 19:41:15 +0100

    Primer commit Eclipse

---
+++ HolaMundo/src/holaMundo/HolaMundo.java
@@ -0,0 +1,10 @@
+package holaMundo;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+		System.out.println("Hola modulo de Entornos de desarrollo");
+	}
+
+}
